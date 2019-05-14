0ff3c39abce11c74b38b419c73a1fb566470ae35
Crowdbotics <team@crowdbotics.com>
2019-05-14 14:38:25 +0000

    initial commit

---
+++ android/app/src/main/java/com/escape_city_app_3331/MainActivity.java
@@ -0,0 +1,15 @@
+package com.escape_city_app_3331;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "escape_city_app_3331";
+    }
+}
